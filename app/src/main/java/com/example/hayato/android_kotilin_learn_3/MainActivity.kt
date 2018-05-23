a5f5c9dd97b98353b0d8e92d90551f3b27af9f6c
hayato <zx.tech.r@gmail.com>
2018-05-23 16:04:45 +0900

    玉転がし

--- app/src/main/java/com/example/hayato/android_kotilin_learn_3/MainActivity.kt
+++ app/src/main/java/com/example/hayato/android_kotilin_learn_3/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.hayato.android_kotilin_learn_3
 
+import android.content.Context
+import android.hardware.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Surface
+import android.view.SurfaceHolder
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +14,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+}
+
+
+class mySurfaceCallBack :AppCompatActivity(),SensorEventListener ,SurfaceHolder.Callback{
+    val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    val accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    var surfaceWidth = 0
+    var surfaceHeight = 0
+
+
+
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event ?: return
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            Log.d("MainActivity",
+                    """
+                    |x = ${event.values[0]}
+                    |y = ${event.values[1]}
+                    |z = ${event.values[2]}
+                """.trimMargin()
+            )
+        }
+    }
+
+
+
+    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+        surfaceWidth = width
+        surfaceHeight = height
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder?) {
+
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder?) {
+        sensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_GAME)
+    }
+
 }
